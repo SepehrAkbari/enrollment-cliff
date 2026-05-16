9be12bdfb3202498301f7d583fad9be0c49de03a
Sepehr Akbari <isepehrakbari@gmail.com>
2026-05-16 16:26:12 -0500

    .

--- data/merger.py
+++ data/merger.py
@@ -1,9 +1,19 @@
 import pandas as pd
 
-colleges_xsl = pd.read_excel('colleges.xlsx')
-states_xsl = pd.read_excel('states.xlsx')
-media_xsl = pd.read_excel('media_articles.xlsx')
+colleges_df = pd.read_csv('colleges.csv')
+states_df = pd.read_csv('states.csv')
+media_df = pd.read_csv('media_articles.csv')
+colleges_df_copy = colleges_df.copy()
+states_df_copy = states_df.copy()
+media_df_copy = media_df.copy()
 
-colleges_xsl.to_csv('colleges.csv', index=False)
-states_xsl.to_csv('states.csv', index=False)
-media_xsl.to_csv('media_articles.csv', index=False)
+states_to_join = states_df_copy.drop(columns=['state'])
+merged_df = pd.merge(colleges_df_copy, states_to_join, on='state_abbr', how='left')
+
+columns_to_drop = ['uid', 'key', 'state', 'year_closed']
+media_to_join = media_df_copy.drop(columns=columns_to_drop, errors='ignore')
+
+final_merged_df = pd.merge(merged_df, media_to_join, on='name', how='left')
+
+final_merged_df.to_csv('merged.csv', index=False)
+final_merged_df.to_excel('merged.xlsx', index=False)
